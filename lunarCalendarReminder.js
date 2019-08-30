48bd857832277dc7fd7e36a6d1d29c4dd246ef1a
lgsoohoo <lgsoohoo@vt.edu>
2019-08-30 01:18:20 -0400

    JS generates proper output for Google Calendar Added Readme/Notes on how to run code  * Generates csv header (only including used fields)  * Generates csv line (only including used fields)  * Calculates end date if event is multiple days long  * Prints csv data in the console as a single output for    easier Copy-Pasting

--- lunarCalendarReminder.js
+++ lunarCalendarReminder.js
@@ -1,116 +1,193 @@
 /*
-    Lunar Calendar Reminder (JS)
-    Lucas Soohoo
-    August, 2019
 
-    This is the JAVASCRIPT version of the "LunarCalendarReminder" program.
-    The PYTHON version is recommended.
+    Lunar Calendar Reminder (JavaScript)
+    Lucas Soohoo, August 2019
 
-    This version of the program can be run in your browser's console
-    and will print out the data in the console, which can be copy-pasted into a
-    CSV file and then imported into Google Calendar.
+    "Lunar Calendar Reminder" allows you to create Google Calendar events for
+    a specific Lunar calendar date. For example, you can enter in a friend or
+    family member's Lunar calendar birthday, and have Google Calendar remind you
+    on the equivalent Gregorian calendar date.
+
+    This JavaScript code can be run in your browser and will print out the data
+    in the console, which can be copy-pasted into a CSV file and imported into
+    Google Calendar.
 
     To use this program:
-    1. Edit program parameters in the <Setup> section
-    2. Open a web browser and go to: https://pteo.paranoiaworks.mobi/chinese_calendar/
-        a. This is required as CORS will block the HTTP requests
-    3. Open the web browser console and copy-paste all of the code
+    1. Edit the program parameters in the <Setup></Setup> section
+    2. Open a web browser and go to:
+        https://pteo.paranoiaworks.mobi/chinese_calendar/
+        a. This is required as CORS will block the HTTP requests if you are on
+           a different page.
+    3. Open the web browser console and copy-paste the code
         a. You may be required to allow pasting into the console
-    4. Open a text editor and paste the header as the first line (without the quotes)
-        a. Header: "Subject, Start Date, Start Time, End Date, End Time, All Day Event, Description, Location, Private"
-    5. Paste the console output into a text editor and save as a ".csv" file
-    5. Upload into Google Calendar
+    5. Copy the console output into a text editor and save as a ".csv" file
+    6. Open Google Calendar and import the CSV file into your calendar
+
+    This is the JAVASCRIPT version of the "Lunar Calendar Reminder" program.
+    You can run this code online at <site coming soon>. If you are writing a
+    script to automate this program, you may consider downloading and running
+    the PYTHON version of this program. <Python version also coming soon.>
+
+    Note: "Lunar Calendar Reminder" is limited to years 1900-2100 because this
+    program relies on the 'Paranoia Works' online converter
+    (https://pteo.paranoiaworks.mobi/chinese_calendar/)
+    to preform Lunar to Gregorian conversions. "Lunar Calendar Reminder" and its
+    creator is not affiliated, endorsed or sponsored by 'Paranoia Works'.
 
 */
 
+
 /*     =====  <Setup>  ======     */
+// Lunar calendar date:
+let lunarDay = 1;
+let lunarMonth = 1;
 
-// Lunar calendar date ====================
-var lunarDay = 1;
-var lunarMonth = 1;
+// Note: This program is limited to years 1900 to 2100
+let startYear = 2019; // Year to start generating event reminders
+let yearsToGenerate = 5; // Number of years to generate
 
-var startYear = 2019; // Year to start generating event reminders
-var yearsToGenerate = 10; // Number of years to generate
-
-// Event Details ====================
+// Event Details:
 // For more info, see: https://support.google.com/calendar/answer/37118
-var csvSubject = ""; // The name of the event, required. Example: Final exam
-var csvStartTime = ""; // The time the event begins. Example: 10:00 AM
-var useEndDate = false; // Whether the event lasts multiple days
-var eventLength = 0; // Number of days the event lasts. Only required if `useEndDate` is `true`
-var csvEndTime = ""; // The time the event ends. Example: 1:00 PM
-var csvAllDayEvent = "True"; // Whether the event is an all-day event. Enter `True` if it is an all-day event, and `False` if it isn't.
-var csvDescription = `Lunar Calendar:${lunarMonth}\\${lunarDay} converted to Gregorian`; // Description or notes about the event. Example: 50 multiple choice questions and two essay questions
-var csvLocation = ""; // The location for the event. Example: "Columbia, Schermerhorn 614"
-var csvPrivate = "True"; // Whether the event should be marked private. Enter `True` if the event is private, and `False` if it isn't.
-
+let eventDetails = [
+    "Chinese New Year", // Subject - The name of the event, required. Example: Chinese New Year
+    // Start Date - The first day of the event, required. Example: 05/30/2020
+    "", // Start Time - The time the event begins. Unnecessary if an 'All Day Event'. Example: 10:00 AM
+    "", // End Time - The time the event ends. Unnecessary if an 'All Day Event'. Example: 1:00 PM
+    "True", // All Day Event - Whether the event is an all-day event. Enter `True` if it is an all-day event, and `False` if it isn't.
+    `Lunar Calendar:${lunarMonth}/${lunarDay} converted to Gregorian`, // Description - Description or notes about the event. Example: 50 multiple choice questions and two essay questions
+    "", // Location - The location for the event. Example: "Columbia, Schermerhorn 614"
+    "True", // Private - Whether the event should be marked private. Enter `True` if the event is private, and `False` if it isn't.
+    0, // Event Length - Number of days the event lasts (after the start date). Enter `0` if the event is only for that one day.
+];
 /*     =====  </Setup>  =====     */
 
+// Program Code ========================================
 
-// CSV Header ====================
-var csvHeader = "Subject, Start Date, Start Time, End Date, End Time, All Day Event, Description, Location, Private";
-// `csvHeader` should not be modified as this exact header is required by Google Calendar
+// Will contain the full CSV file data
+var csvFile;
 
-// Program ====================
-for (let year = startYear; year < startYear + yearsToGenerate; year++) {
-    /* Using `let` keeps visiblity only inside the block. (if you use `var` then the `timeout` doesnt work)
-    In this case, using `var` means that when the `timeout` gets triggered, then each function call
-    will use the value of `year` when the loop has finished, rather than each year as we expect */
+// Remember what event fields we are using
+let usingFields = [
+    eventDetails[0] !== "",
+    // Start Date is always true
+    eventDetails[1] !== "",
+    eventDetails[2] !== "",
+    eventDetails[3] !== "",
+    eventDetails[4] !== "",
+    eventDetails[5] !== "",
+    eventDetails[6] !== "",
+    eventDetails[7] !== 0
+];
 
-    setTimeout(function() {
-        // Small delay so the server won't get overloaded
-        lunarToGregorian(year, lunarMonth, lunarDay);
-    }, (year - startYear) * 1000); //schedule them for [0, 1, 2, ...] seconds after each iteration
 
+// Generate the CSV header (based on what fields are being used)
+function generateHeader() {
+    // Names of headers. This array lines up with the `usingFields` array
+    let headerFields = [
+        "Subject",
+        //"Start Date",
+        "Start Time",
+        "End Time",
+        "All Day Event",
+        "Description",
+        "Location",
+        "Private",
+        "End Date"
+    ];
+
+    let returnHeader = "Start Date"; // CSV will always include the 'Start Date' field
+
+    for (let i = 0; i < usingFields.length; i++) {
+        if (usingFields[i]) {
+            // Include the header for the field if we are using it
+            returnHeader += ", " + headerFields[i];
+        }
+    }
+    return returnHeader;
 }
 
-// Converts a Lunar Calendar date to a Gregorian Calendar date using https://pteo.paranoiaworks.mobi/chinese_calendar
+
+// Generates one line of CSV data (based on what fields are being used)
+function generateCSVLine(year, month, day) {
+    let csvLine = `${month}/${day}/${year}`; // CSV will always include the 'Start Date' field
+
+    for (let i = 0; i < usingFields.length - 1; i++) {
+        // Include the value for the field if we are using it (except for the 'End Date' field)
+        if (usingFields[i]) {
+            csvLine += ", " + eventDetails[i];
+        }
+    }
+
+    if (usingFields[usingFields.length - 1]) {
+        // If the event is longer than one day:
+
+        // Generate the end date
+        // https://stackoverflow.com/a/23081260
+        let startDay = new Date(`${month}/${day}/${year}`);
+        let endDay = new Date(startDay);
+
+        // Set the end date ahead by the given amount of days
+        endDay.setDate(startDay.getDate() + eventDetails[eventDetails.length - 1]);
+
+        let endMonth = endDay.getMonth() + 1; // Months are counted starting at 0
+
+        // Add the end date to csvLine
+        csvLine += ", " + `${endMonth}/${endDay.getDate()}/${endDay.getFullYear()}`;
+    }
+
+    return csvLine;
+}
+
+
+// Converts a Lunar Calendar date to a Gregorian Calendar date using Paranoia Works' website
 function lunarToGregorian(varYear, varMonth, varDay) {
     // Send HTTP requests in a function so the XMLHttpRequest's don't interfere with each other
-    var oReq = new XMLHttpRequest();
+    let oReq = new XMLHttpRequest();
 
-    var link = "https://pteo.paranoiaworks.mobi/chinese_calendar/cncalprov.php?g2c=0&year=" + varYear + "&month=" + varMonth + "&date=" + varDay + "&leap=0";
+    let link = "https://pteo.paranoiaworks.mobi/chinese_calendar/cncalprov.php?g2c=0&year=" + varYear + "&month=" + varMonth + "&date=" + varDay + "&leap=0";
 
     oReq.onreadystatechange = function() {
         if (oReq.readyState == XMLHttpRequest.DONE) {
             if (oReq.status == 500) {
-                // Received an error (serer down because we sent too many requests too quickly)
+                // Received an error. Server is down (probably) because we sent too many requests too quickly
                 // We need to resubmit
-
+                console.log(`Received 500 server error for year: ${varYear}. Resubmitting...`)
                 setTimeout(function() {
-                    // add a 1 second delay to allow server to recover
+                    // Add a 1 second delay to allow server to recover
                     oReq.open("GET", link);
                     oReq.send();
                 }, 1000);
 
             } else {
 
-                var rawRes = oReq.responseText;
+                let rawRes = oReq.responseText;
                 //Possible response formats:
                 // <ERROR>Invalid input date (mm/dd/yyyy)</ERROR>
                 // <ROOT><YEAR>2018</YEAR><MONTH>09</MONTH><DAY>08</DAY><LEAP>0</LEAP>...
 
-                //if (Bad request / Invalid input date) { Don't bother with printing }
+                // Save to CSV only if valid response/input date
                 if (!rawRes.includes("Invalid")) {
 
                     // Extract the Gregorian Calendar date from the xml
                     // https://stackoverflow.com/a/14867897
 
-                    var startIndex = rawRes.lastIndexOf("<YEAR>") + 6; //+6 so it wont print "YEAR>"
-                    var endIndex = rawRes.lastIndexOf("</YEAR>");
-                    var gregYear = rawRes.substring(startIndex, endIndex);
+                    let startIndex = rawRes.lastIndexOf("<YEAR>") + 6; //+6 so it wont print "YEAR>"
+                    let endIndex = rawRes.lastIndexOf("</YEAR>");
+                    let gregYear = rawRes.substring(startIndex, endIndex);
 
                     startIndex = rawRes.lastIndexOf("<MONTH>") + 7;
                     endIndex = rawRes.lastIndexOf("</MONTH>");
-                    var gregMonth = rawRes.substring(startIndex, endIndex);
+                    let gregMonth = rawRes.substring(startIndex, endIndex);
 
                     startIndex = rawRes.lastIndexOf("<DAY>") + 5;
                     endIndex = rawRes.lastIndexOf("</DAY>");
-                    var gregDay = rawRes.substring(startIndex, endIndex);
+                    let gregDay = rawRes.substring(startIndex, endIndex);
 
-                    var gregDate = gregMonth + "/" + gregDay + "/" + gregYear
+                    let csvLine = generateCSVLine(gregYear, gregMonth, gregDay);
 
-                    console.log(csvSubject + ", " + gregDate + ", " + csvStartTime + ", , " + csvEndTime + ", " + csvAllDayEvent + ", " + csvDescription + ", " + csvLocation + ", " + csvPrivate)
+                    // Add the line to the file. We don't use a return statement
+                    // for the function because the HTTP requests are asynchronous
+                    csvFile += csvLine + "\n";
                 }
             }
         }
@@ -120,3 +197,30 @@ function lunarToGregorian(varYear, varMonth, varDay) {
     oReq.send();
 
 } // end lunarToGregorian()
+
+
+// Program Body ========================================
+
+if (startYear < 1900 || (startYear + yearsToGenerate) > 2100) {
+    console.log(`Error: Program is limited to years 1900 to 2100.\nEntered years: ${startYear} to ${startYear+yearsToGenerate}.`);
+} else {
+
+    console.log(`Program is running. Please wait ${yearsToGenerate} seconds.`)
+
+    csvFile = generateHeader() + "\n";
+
+    for (let year = startYear; year < startYear + yearsToGenerate; year++) {
+        setTimeout(function() {
+            // Small delay so the server won't get overloaded
+            lunarToGregorian(year, lunarMonth, lunarDay);
+        }, (year - startYear) * 500);
+        // schedule requests after [0, 0.5, 1, 1.5, 2, ...] seconds
+        // Each request should be roughly 0.5 seconds after the previous
+    }
+
+    setTimeout(function() {
+        // Print CSV file data after all HTTP requests have (probably) finished
+        console.log(csvFile)
+    }, yearsToGenerate * 1000);
+
+}
