7b449f4ee0e1f705a18e237309b1b1154ff0a8b5
lgsoohoo <lgsoohoo@vt.edu>
2019-08-30 21:16:11 -0400

    Added error checking and help message for when CORS blocks HTTP requests Added message when program tries to convert (and skips) invalid dates Shortened delay, minimum of 2 seconds

--- lunarCalendarReminder.js
+++ lunarCalendarReminder.js
@@ -20,6 +20,8 @@
            a different page.
     3. Open the web browser console and copy-paste the code
         a. You may be required to allow pasting into the console
+        b. If you receive a redeclaration error or you are running the code
+        multiple times, paste the code between two braces { } before running
     5. Copy the console output into a text editor and save as a ".csv" file
     6. Open Google Calendar and import the CSV file into your calendar
 
@@ -39,12 +41,12 @@
 
 /*     =====  <Setup>  ======     */
 // Lunar calendar date:
-let lunarDay = 1;
 let lunarMonth = 1;
+let lunarDay = 1;
 
 // Note: This program is limited to years 1900 to 2100
-let startYear = 2019; // Year to start generating event reminders
-let yearsToGenerate = 5; // Number of years to generate
+let startYear = 2019; // Year to start generating event reminders (inclusive)
+let endYear = 2019 + 88; // Year to stop generating event reminders (inclusive)
 
 // Event Details:
 // For more info, see: https://support.google.com/calendar/answer/37118
@@ -54,7 +56,7 @@ let eventDetails = [
     "", // Start Time - The time the event begins. Unnecessary if an 'All Day Event'. Example: 10:00 AM
     "", // End Time - The time the event ends. Unnecessary if an 'All Day Event'. Example: 1:00 PM
     "True", // All Day Event - Whether the event is an all-day event. Enter `True` if it is an all-day event, and `False` if it isn't.
-    `Lunar Calendar:${lunarMonth}/${lunarDay} converted to Gregorian`, // Description - Description or notes about the event. Example: 50 multiple choice questions and two essay questions
+    `Lunar Calendar: ${lunarMonth}/${lunarDay} converted to Gregorian`, // Description - Description or notes about the event. Example: 50 multiple choice questions and two essay questions
     "", // Location - The location for the event. Example: "Columbia, Schermerhorn 614"
     "True", // Private - Whether the event should be marked private. Enter `True` if the event is private, and `False` if it isn't.
     0, // Event Length - Number of days the event lasts (after the start date). Enter `0` if the event is only for that one day.
@@ -65,6 +67,13 @@ let eventDetails = [
 
 // Will contain the full CSV file data
 var csvFile;
+
+// Never got a 0 response from 'Paranoia Works'. Helps stop HTTP requests if
+// errors. See lunarToGregorian() for explanation on 0 HTTP status
+var neverZeroStatus = true;
+
+// Number of milliseconds to delay between HTTP requests
+let httpDelayLength = 100;
 
 // Remember what event fields we are using
 let usingFields = [
@@ -141,6 +150,11 @@ function generateCSVLine(year, month, day) {
 
 // Converts a Lunar Calendar date to a Gregorian Calendar date using Paranoia Works' website
 function lunarToGregorian(varYear, varMonth, varDay) {
+    if (!neverZeroStatus) {
+        // Quit if we got a 0 status earlier
+        return;
+    }
+
     // Send HTTP requests in a function so the XMLHttpRequest's don't interfere with each other
     let oReq = new XMLHttpRequest();
 
@@ -153,11 +167,16 @@ function lunarToGregorian(varYear, varMonth, varDay) {
                 // We need to resubmit
                 console.log(`Received 500 server error for year: ${varYear}. Resubmitting...`)
                 setTimeout(function() {
-                    // Add a 1 second delay to allow server to recover
+                    // Add a small delay to allow server to recover
                     oReq.open("GET", link);
                     oReq.send();
-                }, 1000);
+                }, httpDelayLength);
 
+            } else if (oReq.status == 0) {
+                // Can't reach the 'Paranoia Works' site.
+                // Probably because CORS is blocking it. Could also be that the website is down or user does not have internet connection
+                neverZeroStatus = false;
+                console.log(`Received 0 status for year: ${varYear}.\nEnsure that:\n1. This tab is at the 'Paranoia Works' website\n\thttps://pteo.paranoiaworks.mobi/chinese_calendar/\n2. The 'Paranoia Works' website is up\n3. Your computer is connected to the internet`)
             } else {
 
                 let rawRes = oReq.responseText;
@@ -165,8 +184,11 @@ function lunarToGregorian(varYear, varMonth, varDay) {
                 // <ERROR>Invalid input date (mm/dd/yyyy)</ERROR>
                 // <ROOT><YEAR>2018</YEAR><MONTH>09</MONTH><DAY>08</DAY><LEAP>0</LEAP>...
 
+
                 // Save to CSV only if valid response/input date
-                if (!rawRes.includes("Invalid")) {
+                if (rawRes.includes("Invalid")) {
+                    console.log(`${varMonth}/${varDay} is an invalid date for ${varYear}. Skipping...`)
+                } else {
 
                     // Extract the Gregorian Calendar date from the xml
                     // https://stackoverflow.com/a/14867897
@@ -201,26 +223,35 @@ function lunarToGregorian(varYear, varMonth, varDay) {
 
 // Program Body ========================================
 
-if (startYear < 1900 || (startYear + yearsToGenerate) > 2100) {
-    console.log(`Error: Program is limited to years 1900 to 2100.\nEntered years: ${startYear} to ${startYear+yearsToGenerate}.`);
+if (startYear < 1900 || endYear > 2100) {
+    console.log(`Error: Program is limited to years 1900 to 2100.\nEntered years: ${startYear} to ${endYear}.`);
+} else if (startYear > endYear) {
+    console.log(`Error: Start Year is after End Year.\nEntered years: ${startYear} to ${endYear}.`);
 } else {
 
-    console.log(`Program is running. Please wait ${yearsToGenerate} seconds.`)
+    let printCSVDelayLength = (endYear - startYear + 1) * httpDelayLength * 1.5;
+    if (printCSVDelayLength < 2000) {
+        // delay must be at least 2000ms/2sec
+        printCSVDelayLength = 2000;
+    }
+
+    console.log(`Program is running. Please wait ${Math.round(printCSVDelayLength/1000)} seconds.`)
 
     csvFile = generateHeader() + "\n";
 
-    for (let year = startYear; year < startYear + yearsToGenerate; year++) {
+    for (let year = startYear; year <= endYear; year++) {
         setTimeout(function() {
             // Small delay so the server won't get overloaded
             lunarToGregorian(year, lunarMonth, lunarDay);
-        }, (year - startYear) * 500);
-        // schedule requests after [0, 0.5, 1, 1.5, 2, ...] seconds
-        // Each request should be roughly 0.5 seconds after the previous
+        }, (year - startYear) * httpDelayLength);
+        // schedule requests after [0, 0.1, 0.2, 0.3, 0.4, ...] seconds
+        // Each request should be roughly (httpDelayLength/1000) seconds after the previous
     }
 
     setTimeout(function() {
         // Print CSV file data after all HTTP requests have (probably) finished
-        console.log(csvFile)
-    }, yearsToGenerate * 1000);
+        // Dont print if we got a zero status earlier
+        neverZeroStatus && console.log(csvFile)
+    }, printCSVDelayLength);
 
 }
